b76a3cef06e98b012c0464dacfd16b231fd2dbd7
Liam Brewster <liam.brewster@hotmail.co.uk>
2022-02-12 22:31:19 +0000

    feat: added the footer component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import React, {useState} from "react";
+import axios from "axios";
 import Header from "./components/Header"
+import Footer from "./components/Footer"
+
 
 function App() {
+  //const Url = `https://api.openweathermap.org/data/2.5/weather?q=london&appid=${process.env.REACT_APP_OPEN_WEATHER_APP_ID}`
+
   return (
     <div className="App">
       <Header/>
+      <Footer/>
     </div>
   );
 }
